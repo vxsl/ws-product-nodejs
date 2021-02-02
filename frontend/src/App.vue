c95cf195b22e234189cf05ab528857a058aa0ba0
Kyle Grimsrud-Manz <kyle.grimsrudmanz@gmail.com>
2021-02-02 14:35:50 -0500

    main config, add toaster, misc styling

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,15 @@
 <template>
-  <div id="app">
+  <div id="app" class="bg-light">
+    <b-navbar type="dark" variant="dark" id="nav" class="padded">
+      <img id="logo" src="@/assets/eqworks.svg"/>
+      <div class="d-flex align-items-top">
+        <a class="text-light monospace" href="https://github.com/vxsl/ws-product-nodejs">ws-product-nodejs</a>
+      </div>
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item href="#">Rate Limit Testing Module</b-nav-item>
+        <b-nav-item href="#">Charts</b-nav-item>
+      </b-navbar-nav>
+    </b-navbar>
     <ChartArea :endpoints="endpoints"/>
     <RateLimitTester v-if="false" :endpoints="endpoints"/>
   </div>
@@ -56,21 +66,37 @@ export default {
 </script>
 
 <style lang="scss">
-@use '@/scss/custom.scss';
+@import '@/scss/custom.scss';
 
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Roboto, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   min-height:100vh;
-  //padding:5vh;
+  font-weight:300;
+  font-size:1.1em;
 }
 
 .monospace {
   font-family:'Source Code Pro', 'Courier New', Courier, monospace
 }
 
+.padded {
+  padding-left:9% !important;
+  padding-right:9% !important;
+}
+
+#nav {
+  height:7vh;
+  #logo {
+    padding:0.25vh;
+    filter:invert(100%);
+    max-height:100%;
+    width:auto;
+    margin-right:1em;
+  }
+}
 
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,8 +5,26 @@ import './plugins/bootstrap-vue'
 import App from './App.vue'
 import axios from 'axios';
 
+import { BIconPlusCircle, BIconXCircle } from 'bootstrap-vue'
+Vue.component('b-icon-plus-circle', BIconPlusCircle)
+Vue.component('b-icon-x-circle', BIconXCircle)
+
 Vue.config.productionTip = false
 axios.defaults.baseURL = 'http://localhost:5555'
+
+Vue.mixin({
+  methods: {
+    toast(title, message, autohide = true) {
+      let options = {
+          title: title,
+          toaster: 'b-toaster-bottom-right',
+          noAutoHide: !autohide
+        }
+      autohide? null : options.id = title
+      this.$bvToast.toast(message, options)
+    }
+  }
+})
 
 new Vue({
   render: h => h(App),
