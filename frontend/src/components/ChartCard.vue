3c73397919a30c62d213b32b9e1b10606662df57
Kyle Grimsrud-Manz <kyle.grimsrudmanz@gmail.com>
2021-02-02 17:46:42 -0500

    add more functionality to ChartCard and augment UI

--- frontend/src/components/ChartCard.vue
+++ frontend/src/components/ChartCard.vue
@@ -3,38 +3,45 @@
     <div ref="chartContainer" class="chart-container">
       <b-row class="chart-header">
         <b-col>
-            <span class="chart-title">{{ endpoint.title }}</span>
-            <span class="monospace chart-subtitle">{{endpoint.uri}}</span>
+          <span class="chart-title">{{ endpoint.title }}</span>
+          <span class="monospace chart-subtitle">{{ endpoint.uri }}</span>
         </b-col>
         <b-col class="d-flex justify-content-end align-items-center">
-          <b-button 
-          v-if="dataKeys.length > 1"
-          @click="toggleAbsolute" 
-          class="absolute-button">
-            {{absolute? 'Absolute scale' : 'Relative scale'}}
+          <b-button
+            v-if="dataKeys.length > 1"
+            class="tooltip-button"
+            v-b-tooltip.hover
+            :title="absolute? 'Relative Scale' : 'Absolute Scale'"
+          >
+            <b-icon-graph-up @click="toggleAbsolute" class="grid-button" />
           </b-button>
-          <b-icon-x-circle @click="$emit('close')" class="close-button"/>
+          <b-button class="tooltip-button" v-b-tooltip.hover title="Toggle Grid">
+            <b-icon-grid @click="toggleGrid" class="grid-button" />
+          </b-button>
+          <b-button class="tooltip-button" v-b-tooltip.hover title="Close">
+            <b-icon-x-circle @click="$emit('close')" class="close-button" />
+          </b-button>
         </b-col>
       </b-row>
-      <LineChart v-if="endpoint.chartType == 'line'" ref="chart"/>
-      <BarChart v-else-if="endpoint.chartType == 'bar'" ref="chart"/>
+      <LineChart v-if="endpoint.chartType == 'line'" ref="chart" />
+      <BarChart v-else-if="endpoint.chartType == 'bar'" ref="chart" />
     </div>
   </div>
 </template>
 
 <script>
-const axios = require("axios")
-const { Chart } = require('chart.js')
-import LineChart from "@/components/charts/LineChart.vue"
-import BarChart from "@/components/charts/BarChart.vue"
+const axios = require("axios");
+const { Chart } = require("chart.js");
+import LineChart from "@/components/charts/LineChart.vue";
+import BarChart from "@/components/charts/BarChart.vue";
 
 //eslint-disable-next-line
-import colors from "@/scss/custom.scss"
+import colors from "@/scss/custom.scss";
 
 export default {
   components: {
     LineChart,
-    BarChart
+    BarChart,
   },
   props: {
     endpoint: Object,
@@ -42,162 +49,188 @@ export default {
   data() {
     return {
       dataSource: Object, // to store the result obtained from the API
-      dataKeys:[],  // to store the field(s) that will be displayed as y-axes
-      chartColors:Object.values(colors).splice(0,5),
-      absolute:false,
+      dataKeys: [], // to store the field(s) that will be displayed as y-axes
+      chartColors: Object.values(colors).splice(0, 5),
+      absolute: false,
+      grid: false,
     };
   },
   mounted: async function () {
-
     // make API request
-    this.dataSource = await axios
-      .get(this.endpoint.uri)
-      .then((r) => r.data)
+    this.dataSource = await axios.get(this.endpoint.uri).then((r) => r.data);
 
     // determine which fields are eligible for y-axes and place these strings in dataKeys[]
     Object.entries(this.dataSource[0]).forEach(
-      function([key, val]) {
+      function ([key, val]) {
         if (key !== this.endpoint.xAxis.key && !isNaN(val)) {
-          this.dataKeys.push(key)
+          this.dataKeys.push(key);
         }
-    }.bind(this))
-    
+      }.bind(this)
+    );
+
     // sort data numerically according to the preconfigured 'xAxis.key' value
-    this.dataSource.sort((a, b) => parseFloat(a[this.endpoint.xAxis.key]) - parseFloat(b[this.endpoint.xAxis.key]));
-    
+    this.dataSource.sort(
+      (a, b) =>
+        parseFloat(a[this.endpoint.xAxis.key]) -
+        parseFloat(b[this.endpoint.xAxis.key])
+    );
+
+    // prepare barebones chart config
+    Chart.defaults.global.defaultColor = colors.darkColor;
+    Chart.defaults.scale.gridLines.color = 'transparent';
+
     // create chart (initially relative by default)
-    this.draw(this.absolute)
+    this.draw();
   },
   methods: {
-    toggleAbsolute() {
-      this.absolute = !this.absolute
-      this.draw(this.absolute)
+    toggleGrid() {
+      this.grid = !this.grid;
+      Chart.defaults.global.animation.duration = 0
+      Chart.defaults.scale.gridLines.color = this.grid
+        ? colors.mediumGreyColor
+        : "transparent";
+      this.draw();
     },
-    draw(absolute) {
-
-      // prepare barebones chart config
-      Chart.defaults.global.defaultColor = colors.darkColor;
-      Chart.defaults.scale.gridLines.color = colors.mediumGreyColor;
-      let chartData = { // standard data param
+    toggleAbsolute() {
+      this.absolute = !this.absolute;
+      this.draw();
+    },
+    draw() {
+      let chartData = {
+        // standard data param
         labels: [],
-        datasets: []
-      }
-      let chartOptions = {    // standard options param, with dynamic xAxes configuration.
+        datasets: [],
+      };
+      let chartOptions = {
+        // standard options param, with dynamic xAxes configuration.
         legend: {
           position: "top",
           align: "start",
         },
         responsive: true,
-        maintainAspectRatio:false,
-        scales:{
+        maintainAspectRatio: false,
+        scales: {
           // see @/js/endpointsConfig.js to see what kind of data is being passed to 'scales.xAxes'.
-          xAxes:[this.endpoint.xAxis.config], 
-          yAxes:[]
+          xAxes: [this.endpoint.xAxis.config],
+          yAxes: [],
         },
         elements: {
-            point: {
-                radius: 0.8,
-            },
+          point: {
+            //radius: 0.8,
+          },
         },
-      }
+      };
 
       // create a generator for the primary/secondary/tertiary/etc.. colors defined in @/scss/custom.scss.
-      function* yieldArray(arr) { yield* arr }
-      let chartColorsGen = yieldArray(this.chartColors)
+      function* yieldArray(arr) {
+        yield* arr;
+      }
+      let chartColorsGen = yieldArray(this.chartColors);
 
       // if the 'group' property is set to 'true' for this chart's endpoint in @/js/endpointsConfig.js,
       // replace the retrieved data with a mutated version that is grouped according to the key.
       if (this.endpoint.xAxis.group) {
-        this.dataSource = this.groupData()
+        this.dataSource = this.groupData();
       }
 
       // populate the labels for the x-axis according to the various values of the primary key,
       // like 'hour' or 'date'. Additionally, all numeric values must be cast to Strings in order
-      // to be parsed by moment.js 
-      chartData.labels = this.dataSource.map((obj) => obj[this.endpoint.xAxis.key].toString())
+      // to be parsed by moment.js
+      chartData.labels = this.dataSource.map((obj) =>
+        obj[this.endpoint.xAxis.key].toString()
+      );
 
       // draw the chart with the newly processed data and appropriate scaling
-      absolute? this.drawAbsolute(chartData, chartOptions, chartColorsGen) : this.drawRelative(chartData, chartOptions, chartColorsGen)
+      this.absolute
+        ? this.drawAbsolute(chartData, chartOptions, chartColorsGen)
+        : this.drawRelative(chartData, chartOptions, chartColorsGen);
     },
-    
+
     // ex. for '/hourly/events,' we want to ignore the 'day' field and generate data that shows us how
     // many events occurred at a given hour, regardless of the day. This requires significant manipulation
     // of the original data.
     groupData() {
-      let result = []
-      let groups = new Set()  // ensure uniqueness of each group by collecting in a Set
-      let groupBy = this.endpoint.xAxis.key
-      let dataKeys = this.dataKeys // must define outside loop because VueComponent is not available inside
+      let result = [];
+      let groups = new Set(); // ensure uniqueness of each group by collecting in a Set
+      let groupBy = this.endpoint.xAxis.key;
+      let dataKeys = this.dataKeys; // must define outside loop because VueComponent is not available inside
       for (let obj of this.dataSource) {
-        let el
-        let groupName = obj[groupBy]
+        let el;
+        let groupName = obj[groupBy];
 
         // locate element or create if does not exist, assign to 'el'
         if (groups.has(groupName)) {
-          el = result.find(obj => obj[groupBy] == groupName)
-        }
-        else {
-          groups.add(groupName)
+          el = result.find((obj) => obj[groupBy] == groupName);
+        } else {
+          groups.add(groupName);
           el = {
-            [groupBy]:groupName
-          }
-          result.push(el)
+            [groupBy]: groupName,
+          };
+          result.push(el);
         }
 
         // sum values of duplicate fields
         for (let k of dataKeys) {
-          el[k] = el[k]? el[k] + parseFloat(obj[k]) : parseFloat(obj[k])
+          el[k] = el[k] ? el[k] + parseFloat(obj[k]) : parseFloat(obj[k]);
         }
       }
-      return result
+      return result;
     },
     // augment the passed data and options params as required for an absolute-scale chart.
     drawAbsolute(data, options, colors) {
       // simply create a dataset for each dataKey.
       this.dataKeys.forEach(
-        function(key) {
-          let id = key.charAt(0).toUpperCase() + key.slice(1)
+        function (key) {
+          let id = key.charAt(0).toUpperCase() + key.slice(1);
           data.datasets.push({
-            label:id,
-            backgroundColor:colors.next().value, 
-            data:this.dataSource.map((obj) => obj[key]),
-          })         
-      }.bind(this))
+            label: id,
+            backgroundColor: colors.next().value,
+            data: this.dataSource.map((obj) => obj[key]),
+          });
+        }.bind(this)
+      );
 
       // finally,draw the chart
-      this.$refs.chart.draw(data, options)
+      this.$refs.chart.renderChart(data, options);
     },
     // augment the passed data and options params as required for a relative-scale chart.
     drawRelative(data, options, colors) {
       // create a dataset and y axis for each dataKey, correlate with id
       this.dataKeys.forEach(
-        function(key) {
-          let id = key.charAt(0).toUpperCase() + key.slice(1)
-          let color = colors.next().value // get next color from generator
+        function (key) {
+          let id = key.charAt(0).toUpperCase() + key.slice(1);
+          let color = colors.next().value; // get next color from generator
           options.scales.yAxes.push({
-            id:id,
-            type:'linear',
-            position:'left',
-            ticks: {  // ensure that the scale is relative to the range of the dataset.
-              max:Math.max.apply(Math, this.dataSource.map(o => o[key])),
-              min:Math.min.apply(Math, this.dataSource.map(o => o[key])),
-              fontColor:color
-            }
-          })
+            id: id,
+            type: "linear",
+            position: "left",
+            ticks: {
+              // ensure that the scale is relative to the range of the dataset.
+              max: Math.max.apply(
+                Math,
+                this.dataSource.map((o) => o[key])
+              ),
+              min: Math.min.apply(
+                Math,
+                this.dataSource.map((o) => o[key])
+              ),
+              fontColor: color,
+            },
+          });
           data.datasets.push({
-            label:id,
-            backgroundColor:color,
-            data:this.dataSource.map((obj) => obj[key]),
-            yAxisID:id
-          })         
-      }.bind(this))
+            label: id,
+            backgroundColor: color,
+            data: this.dataSource.map((obj) => obj[key]),
+            yAxisID: id,
+          });
+        }.bind(this)
+      );
 
       // finally, draw the chart
-      this.$refs.chart.draw(data, options)
+      this.$refs.chart.renderChart(data, options);
     },
-  }
+  },
 };
-
 </script>
 
 <style lang="scss">
@@ -209,42 +242,47 @@ canvas {
   -ms-user-select: none;
 }
 .chart-outer-container {
-  background:$light-grey-color;
-  border-radius:3em;
+  background: $light-grey-color;
+  border-radius: 3em;
   position: relative;
-  height:100%;
-  width:100%;
+  height: 100%;
+  width: 100%;
   margin: 1em;
   user-select: none;
-  margin:0;
+  margin: 0;
 
   .chart-container {
     filter: none;
     transition: filter 0.5s;
     overflow: hidden;
     padding: 3em;
-    padding-top:1.5em;
+    padding-top: 1.5em;
     .chart-header {
       text-align: left;
       * {
-        display:block;
+        display: block;
       }
-      margin-bottom:1em;
+      margin-bottom: 1em;
       .absolute-button {
-        background:transparent;
-        border:solid;
-        border-width:1px;
-        border-color:$grey-color;
-        color:$grey-color;
+        background: transparent;
+        border: solid;
+        border-width: 1px;
+        border-color: $grey-color;
+        color: $grey-color;
       }
-      .close-button {
-        cursor:pointer;
-        font-size:1.3em;
-        margin-left:1em;
-        margin-right:1em;
+      .tooltip-button {
+        margin-left: 1em;
+        margin-right: 1em;
+        background: transparent;
+        border: none;
+        color: $dark-color;
+      }
+      .close-button,
+      .grid-button {
+        cursor: pointer;
+        font-size: 1.3em;
       }
     }
   }
 }
-
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,9 +6,13 @@ import App from './App.vue'
 import axios from 'axios';
 import router from './router'
 
-import { BIconPlusCircle, BIconXCircle } from 'bootstrap-vue'
+import { BIconPlusCircle, BIconXCircle, BIconThreeDots, BIconArrowDown, BIconGrid3x3, BIconGraphUp } from 'bootstrap-vue'
 Vue.component('b-icon-plus-circle', BIconPlusCircle)
 Vue.component('b-icon-x-circle', BIconXCircle)
+Vue.component('b-icon-three-dots', BIconThreeDots)
+Vue.component('b-icon-arrow-down', BIconArrowDown)
+Vue.component('b-icon-grid', BIconGrid3x3)
+Vue.component('b-icon-graph-up', BIconGraphUp)
 
 Vue.config.productionTip = false
 axios.defaults.baseURL = 'http://localhost:5555'
