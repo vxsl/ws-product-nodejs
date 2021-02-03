90ac4fa2bb03c845d18adeccf34d737439c05386
Kyle Grimsrud-Manz <kyle.grimsrudmanz@gmail.com>
2021-02-03 03:55:06 -0500

    augment mixin for Geo

--- frontend/src/mixins/dataOperations.js
+++ frontend/src/mixins/dataOperations.js
@@ -1,36 +1,48 @@
 const moment = require("moment");
 
 export default {
-  name:'dataOperations',
+  name: 'dataOperations',
   methods: {
     // ex. for '/hourly/events,' we want to ignore the 'day' field and generate data that shows us how
     // many events occurred at a given hour, regardless of the day. This requires significant manipulation
     // of the original data.
-    groupData (groupBy, dataKeys, dataSource) {
-        let result = [];
-        let groups = new Set(); // ensure uniqueness of each group by collecting in a Set
-        for (let obj of dataSource) {
-            let el;
-            let groupName = obj[groupBy];
+    groupData(groupBy, dataKeys, dataSource) {
+      let result = [];
+      let groups = new Set(); // ensure uniqueness of each group by collecting in a Set
+      for (let obj of dataSource) {
+        let el;
+        let groupName = obj[groupBy];
 
-            // locate element or create if does not exist, assign to 'el'
-            if (groups.has(groupName)) {
-            el = result.find((obj) => obj[groupBy] == groupName);
-            } else {
-            groups.add(groupName);
-            el = {
-                [groupBy]: groupName,
-            };
-            result.push(el);
-            }
-
-            // sum values of duplicate fields
-            for (let k of dataKeys) {
-            el[k] = el[k] ? el[k] + parseFloat(obj[k]) : parseFloat(obj[k]);
-            }
+        // locate element or create if does not exist, assign to 'el'
+        if (groups.has(groupName)) {
+          el = result.find((obj) => obj[groupBy] == groupName);
+        } else {
+          groups.add(groupName);
+          el = {
+            [groupBy]: groupName,
+          };
+          result.push(el);
         }
-        return result;
-    }
+
+        // sum values of duplicate fields
+        for (let k of dataKeys) {
+          el[k] = el[k] ? el[k] + parseFloat(obj[k]) : parseFloat(obj[k]);
+        }
+      }
+      return result;
+    },
+    groupDataForMap(groupBy, dataSource, parameter) {
+      let result = [];
+      for (let obj of dataSource) {
+        let groupName = obj[groupBy];
+        if (result[groupName]) {
+          result[groupName] += obj[parameter]
+        } else {
+          result[groupName] = obj[parameter]
+        }
+      }
+      return result;
+    },
     parseKey(key) {
       let parser = this.endpoint.xAxis.config.time.parser;
       let format = this.endpoint.xAxis.config.time.displayFormats.hour;
